ce0d59ee7c67b7b60c52c0fadf0dc74a86a2ca06
mkrill <michael.krill@telekom.de>
2019-11-27 16:48:20 +0100

    Implemented collection 02 chanllenge

--- Thanos.java
+++ Thanos.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Thanos {
@@ -13,13 +15,29 @@ public class Thanos {
 
         // TODO 1 : Modify Hero to implements Comparable and sort by name (ascending)
 
+        Collections.sort(heroes);
 
         System.out.println("\nOrder by name:");
         showList(heroes);
 
         // TODO 2: Add a Comparator and sort by age (descending)
 
-
+        // Comparator by age
+        Comparator<Hero> heroAgeComparator = new Comparator<Hero>() {
+            @Override
+            public int compare(Hero h1, Hero h2) {
+                if (h1.getAge() > h2.getAge()) {
+                	return -1;
+                } else if (h1.getAge() == h2.getAge()) {
+                	return 0;
+                } else {
+                	return 1;
+                }
+            }
+        };
+        
+        Collections.sort(heroes, heroAgeComparator);
+        
         System.out.println("\nOrder by age:");
         showList(heroes);
     }
